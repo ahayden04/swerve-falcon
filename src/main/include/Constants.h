2ff2b0ba0293dd970f3c535f96ce91532b8f0363
Adrian Hayden <adrian@littlehayden.net>
2022-02-17 23:46:20 -0500

    Update drivetrainConstants.
    Now has distinction between drive and turn motors; correct number of addresses.

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -20,10 +20,16 @@ namespace controllerConstants {
 namespace drivetrainConstants {
 
     //CAN IDs
-    constexpr int motorRearRight = 0;
-    constexpr int motorFrontRight = 1;
-    constexpr int motorRearLeft = 2;
-    constexpr int motorFrontLeft = 3;
+    constexpr int motorDriveFrontRight = 0;
+    constexpr int motorDriveRearRight = 2;
+    constexpr int motorDriveFrontLeft = 4;
+    constexpr int motorDriveRearLeft = 6;
+
+    constexpr int motorTurnFrontRight = 1;
+    constexpr int motorTurnRearRight = 3;
+    constexpr int motorTurnFrontLeft = 5;
+    constexpr int motorTurnRearLeft = 7;
+
 
     constexpr double motorMaxOutput = 0.5;
     constexpr double motorDeadband = 0.1;
