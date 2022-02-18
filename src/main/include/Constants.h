ba028dc285f788eda57485a44a0c63d7e59e52ee
Adrian Hayden <adrian@littlehayden.net>
2022-02-17 20:30:19 -0500

    Update Constants.h
    Created controller and drivetrain namespaces; formatting.

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -1,15 +1,31 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #pragma once
 
 /**
- * The Constants header provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants.  This should not be used for any other
- * purpose.
- *
- * It is generally a good idea to place constants into subsystem- or
- * command-specific namespaces within this header, which can then be used where
- * they are needed.
+ * This header contains hold robot-wide numerical or boolean constants ONLY.
+ * 
+ * Place constants into subsystem/command -specific NAMESPACES within this
+ * header, which can then be included (where they are needed).
  */
+
+constexpr int PLACEHOLDER_INT = 0;
+
+namespace controllerConstants {
+
+    //USB port addresses on drivestation PC.
+    constexpr int controllerMain = 0;
+    constexpr int controllerAux = 1;
+
+}
+
+namespace drivetrainConstants {
+
+    //CAN IDs
+    constexpr int motorRearRight = 0;
+    constexpr int motorFrontRight = 1;
+    constexpr int motorRearLeft = 2;
+    constexpr int motorFrontLeft = 3;
+
+    constexpr double motorMaxOutput = 0.5;
+    constexpr double motorDeadband = 0.1;
+
+}
