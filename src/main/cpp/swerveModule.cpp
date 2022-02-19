09c62da79bc6f765e997a9d486256e84c3a968a0
Adrian Hayden <adrian@littlehayden.net>
2022-02-19 11:05:38 -0500

    Initial FRC PID code.
    Header file essentially complete. Still needs work in cpp file.

--- src/main/cpp/swerveModule.cpp
+++ src/main/cpp/swerveModule.cpp
@@ -1,3 +1,7 @@
 #include "swerveModule.h"
 
-swerveModule::swerveModule() {}
+#include <frc/geometry/Rotation2d.h>
+#include <wpi/numbers>
+
+swerveModule::swerveModule(const int motorDrive, const int motorTurn, const int encoderTurn)
+    : m_motorDrive(motorDrive), m_motorTurn(motorTurn), m_encoderTurn(encoderTurn) {}
--- src/main/include/swerveModule.h
+++ src/main/include/swerveModule.h
@@ -1,10 +1,40 @@
 #pragma once
 
+#include <ctre/phoenix/sensors/CANCoder.h>
+#include <ctre/Phoenix.h>
+#include <frc/controller/PIDController.h>
+#include <frc/controller/ProfiledPIDController.h>
+#include <frc/controller/SimpleMotorFeedforward.h>
+#include <frc/kinematics/SwerveModuleState.h>
+#include <units/angular_velocity.h>
+#include <units/time.h>
+#include <units/velocity.h>
+#include <units/voltage.h>
+#include <wpi/numbers>
+
 class swerveModule {
  public:
-  swerveModule();
+  swerveModule(int motorDrive, int motorTurn, int encoderTurn);
+
+  frc::SwerveModuleState GetState() const;
+
+  void SetDesiredState(const frc::SwerveModuleState& state);
 
  private:
-  // Components (e.g. motor controllers and sensors) should generally be
-  // declared private and exposed only through public methods.
+  WPI_CANCoder m_encoderTurn;
+  WPI_TalonFX m_motorDrive;
+  WPI_TalonFX m_motorTurn;
+
+  static constexpr auto moduleMaxAngularVelocity =
+      wpi::numbers::pi * 1_rad_per_s;  // radians per second
+  static constexpr auto moduleMaxAngularAcceleration =
+      wpi::numbers::pi * 2_rad_per_s / 1_s;  // radians per second^2
+
+  frc2::PIDController m_drivePIDController{1, 0, 0};
+  frc::ProfiledPIDController<units::radians> m_turnPIDController{
+    0, 0, 0,
+    {moduleMaxAngularVelocity, moduleMaxAngularAcceleration}};
+  
+  frc::SimpleMotorFeedforward<units::meters> m_driveFeedforward{1_V, 3_V / 1_mps};
+  frc::SimpleMotorFeedforward<units::radians> m_turnFeedforward{1_V, 0.5_V / 1_rad_per_s};
 };
