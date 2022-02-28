1f2738b0d2fe09f9ac0477e50fbdaf8de81eb031
Adrian Hayden <adrian@littlehayden.net>
2022-02-28 16:14:34 -0500

    Add kinematics object, and set modules using its outputs

--- src/main/cpp/subsystems/drivetrain.cpp
+++ src/main/cpp/subsystems/drivetrain.cpp
@@ -4,6 +4,25 @@ drivetrain::drivetrain() {
     m_navX.ZeroYaw();
 }
 
+void drivetrain::SwerveDrive(units::meters_per_second_t xSpeed,
+                             units::meters_per_second_t ySpeed,
+                             units::radians_per_second_t zRot,
+                             bool fieldRelative) {
+    auto moduleStates = m_kinematics.ToSwerveModuleStates(
+        fieldRelative ? frc::ChassisSpeeds::FromFieldRelativeSpeeds(
+                            xSpeed, ySpeed, zRot, m_navX.GetRotation2d())
+                      : frc::ChassisSpeeds{xSpeed, ySpeed, zRot});
+    //ADD FINAL MAX SPEED, TEMP 0_mps
+    m_kinematics.DesaturateWheelSpeeds(&moduleStates, 0_mps);
+
+    auto [frontRight, rearRight, frontLeft, rearLeft] = moduleStates;
+
+    m_frontRight.SetDesiredState(frontRight);
+    m_rearRight.SetDesiredState(rearRight);
+    m_frontLeft.SetDesiredState(frontLeft);
+    m_rearLeft.SetDesiredState(rearLeft);
+}
+
 void drivetrain::Periodic() {}
 
 void drivetrain::SimulationPeriodic() {}
--- src/main/include/subsystems/drivetrain.h
+++ src/main/include/subsystems/drivetrain.h
@@ -1,16 +1,24 @@
 #pragma once
 
 #include <AHRS.h>
+#include <frc/geometry/Translation2d.h>
+#include <frc/kinematics/SwerveDriveKinematics.h>
 #include <frc2/command/SubsystemBase.h>
-
+#include <units/length.h>
 
 #include "swerveModule.h"
 #include "Constants.h"
+
 using namespace drivetrainConstants::swerveModules;
 
 class drivetrain : public frc2::SubsystemBase {
  public:
   drivetrain();
+
+  void SwerveDrive(units::meters_per_second_t xSpeed,
+                   units::meters_per_second_t ySpeed,
+                   units::radians_per_second_t zRot,
+                   bool fieldRelative);
 
   /**
    * Will be called periodically whenever the CommandScheduler runs.
@@ -25,6 +33,17 @@ class drivetrain : public frc2::SubsystemBase {
 
  private:
   AHRS m_navX{frc::SPI::kMXP};
+
+  //GET FINAL VALUES, TEMP 0_in
+  frc::Translation2d m_locationFrontRight{+0_in, -0_in};
+  frc::Translation2d m_locationRearRight{-0_in, -0_in};
+  frc::Translation2d m_locationFrontLeft{+0_in, +0_in};
+  frc::Translation2d m_locationRearLeft{-0_in, +0_in};
+
+  frc::SwerveDriveKinematics<4> m_kinematics{m_locationFrontRight,
+                                             m_locationRearRight,
+                                             m_locationFrontLeft,
+                                             m_locationRearLeft};
 
   swerveModule m_frontRight{moduleFrontRight};
   swerveModule m_rearRight{moduleRearRight};
